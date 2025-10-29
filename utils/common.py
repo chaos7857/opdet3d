009f59b8bf628606a598664dfdaabc9bc70f596e
cc_if <173192386@qq.com>
2025-10-29 10:15:09 +0800

    init

---
+++ utils/common.py
@@ -0,0 +1,32 @@
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S'
+)
+##########################################################
+import json
+def dump_json(data,filename:str)->bool:
+    try:
+        # dir_name = os.path.dirname(filename)
+        # os.makedirs(dir_name,exist_ok=True)
+        with open(filename,'w', encoding='utf-8') as f:
+            json.dump(data,f,ensure_ascii=False, indent=2)
+        return True
+    except Exception as e:
+        print(e)
+        return False
+def load_small_json(json_name:str):
+    with open(json_name,'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data
+############################################################
+def err_control(func):
+    def wrapper(*args,**kwargs):
+        try:
+            return func(*args,**kwargs)
+        except Exception as e:
+            logging.error(e)
+            return None
+    return wrapper
+############################################################
